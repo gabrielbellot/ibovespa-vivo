385390058f2b1ba65a34867711214985a31134bc
gabrielbellot <gpiresbellot@gmail.com>
2020-03-30 23:22:41 -0300

    Mandar mensagens na abertura e no fechamento do mercado

--- src/main/kotlin/net/mrgaabriel/ibovespavivo/IbovespaVivo.kt
+++ src/main/kotlin/net/mrgaabriel/ibovespavivo/IbovespaVivo.kt
@@ -14,10 +14,13 @@ import twitter4j.TwitterFactory
 import twitter4j.conf.ConfigurationBuilder
 import java.io.File
 import java.text.DecimalFormat
+import java.time.DayOfWeek
 import java.time.LocalDate
 import java.time.OffsetDateTime
 import java.time.format.DateTimeFormatter
+import kotlin.concurrent.thread
 import kotlin.math.absoluteValue
+import kotlin.math.roundToInt
 
 
 object IbovespaVivo {
@@ -62,48 +65,111 @@ object IbovespaVivo {
         else
             null
 
-        while (true) {
-            val quote = getQuote()
-            val today = LocalDate.now()
+        thread {
+            while (true) {
+                val now = OffsetDateTime.now()
 
-            if (quote.latestTradingDay != today) {
+                val quote = getQuote()
+
+                // Abertura da Bovespa
+                if (now.hour == 10 && now.minute == 0 && now.second == 0) {
+                    if (now.dayOfWeek == DayOfWeek.SATURDAY || now.dayOfWeek == DayOfWeek.SUNDAY)
+                        continue
+
+                    logger.info { "Mandando mensagens de abertura da bolsa..." }
+
+                    val msg = """
+                        🕙 Abertura da Bovespa (${now.format(DateTimeFormatter.ofPattern("dd/MM/YYYY"))})
+                        
+                        Posição atual: ${quote.price}
+                        Fechamento anterior: ${quote.previousClose} (${quote.changePercent}%) - (${quote.latestTradingDay.format(DateTimeFormatter.ofPattern("dd/MM/YYYY"))})
+                    """.trimIndent()
+
+                    twitter?.updateStatus(msg)?.also {
+                        logger.info { "Enviado no Twitter! ${it.url()}" }
+                    }
+
+                    telegram?.execute(SendMessage("@${config.telegram.channel}", msg))?.also {
+                        logger.info { "Enviado no Telegram!" }
+                    }
+
+                    Thread.sleep(1000)
+                    continue
+                }
+
+                // Fechamento da Bovespa
+                if (now.hour == 18 && now.minute == 30 && now.second == 0) {
+                    if (now.dayOfWeek == DayOfWeek.SATURDAY || now.dayOfWeek == DayOfWeek.SUNDAY)
+                        continue
+
+                    logger.info { "Mandando mensagens de fechamento da bolsa..." }
+
+                    val msg = """
+                        🕡 Fechamento da Bovespa (${now.format(DateTimeFormatter.ofPattern("dd/MM/YYYY"))})
+                        
+                        Posição atual: ${quote.price} (${quote.changePercent}%)
+                        Fechamento anterior: ${quote.previousClose} - (${quote.latestTradingDay.format(DateTimeFormatter.ofPattern("dd/MM/YYYY"))})
+                    """.trimIndent()
+
+                    twitter?.updateStatus(msg)?.also {
+                        logger.info { "Enviado no Twitter! ${it.url()}" }
+                    }
+
+                    telegram?.execute(SendMessage("@${config.telegram.channel}", msg))?.also {
+                        logger.info { "Enviado no Telegram!" }
+                    }
+
+                    Thread.sleep(1000)
+                    continue
+                }
+            }
+        }
+
+        thread {
+            while (true) {
+                val quote = getQuote()
+                val today = LocalDate.now()
+
+                if (quote.latestTradingDay != today) {
+                    Thread.sleep(3 * 60 * 1000)
+                    continue
+                }
+
+                logger.info { "Rate: ${quote.price}" }
+                logger.info { "É diferente de $lastRate? ${lastRate != quote.price}" }
+
+                if (lastRate == quote.price) {
+                    Thread.sleep(3 * 60 * 1000)
+                    continue
+                }
+
+                logger.info { "A diferença é maior que 2 pontos? ${(quote.price - lastRate).absoluteValue > 2}" }
+
+                if ((quote.price - lastRate).absoluteValue > 2) {
+                    Thread.sleep(3 * 60 * 1000)
+                    continue
+                }
+
+                logger.info { "Atualizando status!" }
+
+                val now = OffsetDateTime.now()
+                val timestamp = now.format(DateTimeFormatter.ofPattern("HH:mm"))
+
+                val msg =
+                    "${if (quote.price > quote.open) "\uD83D\uDCC8" else "\uD83D\uDCC9"} ${quote.price} pontos (${quote.changePercent}%) - ás $timestamp"
+
+                twitter?.updateStatus(msg)?.also {
+                    logger.info { "Postado no Twitter! ${it.url()}" }
+                }
+
+                telegram?.execute(SendMessage("@${config.telegram.channel}", msg))?.also {
+                    logger.info { "Enviado no Telegram!" }
+                }
+
+                lastRate = quote.price
+
                 Thread.sleep(3 * 60 * 1000)
-                continue
             }
-            
-            logger.info { "Rate: ${quote.price}" }
-            logger.info { "É diferente de $lastRate? ${lastRate != quote.price}" }
-
-            if (lastRate == quote.price) {
-                Thread.sleep(3 * 60 * 1000)
-                continue
-            }
-
-            logger.info { "A diferença é maior que 2 pontos? ${(quote.price - lastRate).absoluteValue > 2}" }
-
-            if ((quote.price - lastRate).absoluteValue > 2) {
-                Thread.sleep(3 * 60 * 1000)
-                continue
-            }
-
-            logger.info { "Atualizando status!" }
-
-            val now = OffsetDateTime.now()
-            val timestamp = now.format(DateTimeFormatter.ofPattern("HH:mm"))
-
-            val msg = "${if (quote.price > quote.open) "\uD83D\uDCC8" else "\uD83D\uDCC9"} ${quote.price} pontos (${quote.changePercent}%) - ás $timestamp"
-
-            twitter?.updateStatus(msg)?.also {
-                logger.info { "Postado no Twitter! ${it.url()}" }
-            }
-
-            telegram?.execute(SendMessage("@${config.telegram.channel}", msg))?.also {
-                logger.info { "Enviado no Telegram!" }
-            }
-
-            lastRate = quote.price
-
-            Thread.sleep(3 * 60 * 1000)
         }
     }
 
@@ -159,7 +225,7 @@ class Quote(
     val low = limitDecimal(globalQuote["04. low"].asDouble)
     val price = limitDecimal(globalQuote["05. price"].asDouble)
     val volume = globalQuote["06. volume"].asBigInteger
-    val latestTradingDay = try { LocalDate.parse(globalQuote["07. latest trading day"].asString) } catch (e: Exception) {/* whatever */}
+    val latestTradingDay = LocalDate.parse(globalQuote["07. latest trading day"].asString)
     val previousClose = limitDecimal(globalQuote["08. previous close"].asDouble)
     val change = limitDecimal(globalQuote["09. change"].asDouble)
 
